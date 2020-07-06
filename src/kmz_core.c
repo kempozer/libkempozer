4c0af0e11d43e68fac5cd6d7b4fe4b4d22697d55
Bryan Wofter <bryanwofter@gmail.com>
2020-07-06 11:44:46 -0400

    Implemented _KmzCoreStdMatrix and _KmzCoreStdImage types

---
+++ src/kmz_core.c
@@ -0,0 +1,9 @@
+//
+//  kmz_core.c
+//  checker
+//
+//  Created by Bryan Wofter on 7/6/20.
+//  Copyright © 2020 dirtypixels. All rights reserved.
+//
+
+#include "kmz_core.h"
---
+++ src/kmz_core.h
@@ -0,0 +1,14 @@
+//
+//  kmz_core.h
+//  checker
+//
+//  Created by Bryan Wofter on 7/6/20.
+//  Copyright © 2020 dirtypixels. All rights reserved.
+//
+
+#ifndef kmz_core_h
+#define kmz_core_h
+
+#include <stdio.h>
+
+#endif /* kmz_core_h */
